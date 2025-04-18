6cdb629524603a29bbfc8633eafb75382f749746
thebocher <qurka.kolika@gmail.com>
2025-04-18 11:22:01 +0200

    implement all

--- src/Containers.Application/ContainerService.cs
+++ src/Containers.Application/ContainerService.cs
@@ -1,6 +1,52 @@
-﻿namespace Containers.Application;
+﻿using Containers.Models;
+using Microsoft.Data.SqlClient;
+
+namespace Containers.Application;
 
 public class ContainerService
 {
+    private string _connectionString;
     
+    public ContainerService(string connectionString)
+    {
+        _connectionString = connectionString;        
+    }
+    
+    public IEnumerable<Container> GetAllContainers()
+    {
+        List<Container> containers = [];
+        const string queryString = "SELECT * FROM Containers";
+
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            SqlCommand command = new SqlCommand(queryString, connection);
+            
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            try
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        var containerRow = new Container
+                        {
+                            Id = reader.GetInt32(0),
+                            ContainerTypeId = reader.GetInt32(1),
+                            IsHazardous = reader.GetBoolean(2),
+                            Name = reader.GetString(3),
+                        };
+                        containers.Add(containerRow);
+                    }
+                }
+                
+                return containers;
+            }
+            finally 
+            {
+                reader.Close();
+            }
+        }
+    }
 }
--- src/Containers.Models/Container.cs
+++ src/Containers.Models/Container.cs
@@ -2,5 +2,8 @@
 
 public class Container
 {
-    
+    public int Id { get; set; }
+    public int ContainerTypeId { get; set; }
+    public bool IsHazardous { get; set; }
+    public string Name { get; set; }
 }
